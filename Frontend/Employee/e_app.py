9ffe16feeb183c055536911cc72ef15be251d6f7
Zubin Shah <wcoderzs@gmail.com>
2023-07-22 01:20:04 +0530

    progress overVisualization

--- Frontend/Employee/e_app.py
+++ Frontend/Employee/e_app.py
@@ -4,12 +4,80 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 
+
+import random
+
+import findspark
+findspark.init()
+findspark.find()
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+
+
+
 # Step 1: Initialize the Dash app
 app = dash.Dash(__name__)
 
 # Step 2: Load data (optional)
 # Assuming you have a CSV file named 'data.csv' with the required data
-data = pd.read_csv('data.csv')
+data = pd.read_csv('reviews_data.csv')
+
+
+month = 6
+year = 2000
+group = 'Book'
+months = []
+counts = []
+profit = []
+
+
+def plotData(reviews, group, year=2000, month=6):
+  filtered_reviews = []
+  for review in reviews:
+    if review['group'] == group and review['year'] == year and review['month'] == month:
+      filtered_reviews.append(review)
+
+  # from pyspark.sql import SparkSession
+  spark = SparkSession.builder.getOrCreate()
+  reviews_df = spark.createDataFrame(filtered_reviews)
+
+  reviews_df.createOrReplaceTempView("reviews_table")
+  query = "SELECT day, group, SUM(profit) as daily_profit, COUNT(*) as count FROM reviews_table GROUP BY day, group ORDER BY day"
+  reviews_count_per_day = spark.sql(query)
+
+
+  group_reviews = reviews_count_per_day.filter(reviews_count_per_day.group == group)
+
+  group_reviews_month_count = group_reviews.select('day', 'count','daily_profit')
+  # Collect the rows as a list of dictionaries
+  result_rows = group_reviews_month_count.collect()
+
+  # Extract months and counts from the rows
+  months = [row['day'] for row in result_rows]
+  counts = [row['count'] for row in result_rows]
+  profit = [row['daily_profit'] for row in result_rows]
+  # Plot the data
+
+
+
+def loadData():
+  import csv
+  reviews = []
+  with open('reviews_data.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+      reviews.append(row)
+
+  plotData(reviews,group,year,month)
+
+
+
+  return reviews
+
+loadData()
+
+
+
 
 # Step 3: Design the layout with custom CSS
 app.layout = html.Div(
@@ -65,10 +133,12 @@ app.layout = html.Div(
                 id='example-graph',
                 figure={
                     'data': [
-                        {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                        {'x': months, 'y': counts, 'type': 'bar', 'name': 'SF'},
                     ],
                     'layout': {
-                        'title': 'Bar chart example'
+                        'title': 'Bar chart example',
+                        'xaxis': {'title': 'X Axis'},
+                        'yaxis': {'title': 'Y Axis'}
                     }
                 },
                 style={'display': 'inline-block', 'width': '45%', 'marginRight': '5%'}  # Reduce the width and add margin
