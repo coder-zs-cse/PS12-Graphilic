4205115f61e5c39bf823c4e0e0039f2ca1281781
Omkar Borker <omkarborker15@gmail.com>
2023-07-21 22:34:53 +0530

    added pages frontend

--- Frontend/User/src/components/SearchResults.jsx
+++ Frontend/User/src/components/SearchResults.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SearchResults = ({ searchResults }) => {
   return (
     <div className="search-results-container">
       <h2>Search Results</h2>
-      <br></br>
       {searchResults.map((result, index) => (
-        <div key={index} className="search-result">
-          <img
-            src={`https://stacks-production-us-east-1-upload.imgix.net/38cd183a-3977-429c-971b-2d48285e992f.jpg?w=&h=280&dpr=3&auto=format&auto=compress&codec=mozjpeg&cs=strip`}
-            alt={`Book ${index + 1}`}
-            className="book-image"
-          />
-          <h3>{result.title}</h3>
-          <p>Category: {result.category}</p>
-          <p>Categories: {result.categories.join(', ')}</p>
-        </div>
+        <Link to={`/ProductPage/${result.id}`} key={index} className="product-link">
+          <div className="search-result">
+            <img
+              src={`https://placeimg.com/200/300/book?${index}`}
+              alt={`Book ${index + 1}`}
+              className="book-image"
+            />
+            <h3>{result.title}</h3>
+            <p>Category: {result.category}</p>
+            <p>Categories: {result.categories.join(', ')}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
