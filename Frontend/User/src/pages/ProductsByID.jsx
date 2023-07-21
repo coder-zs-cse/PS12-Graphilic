6cb6df4e83e0de2ddd5e8c91eef435ba20ebabbd
Omkar Borker <omkarborker15@gmail.com>
2023-07-22 04:02:41 +0530

    added recommendation system

--- Frontend/User/src/pages/ProductsByID.jsx
+++ Frontend/User/src/pages/ProductsByID.jsx
@@ -1,36 +1,22 @@
 import React from 'react';
 
 const ProductsPage = ({ recommendation }) => {
-  // Replace this with your actual product data or fetch the product details from the backend
-  const productDetails = {
-    "157794349X": {
-      title: "Recommended Product 1",
-      category: "Recommended Category 1",
-    },
-    "0892749504": {
-      title: "Recommended Product 2",
-      category: "Recommended Category 2",
-    },
-    "1577941829": {
-      title: "Recommended Product 3",
-      category: "Recommended Category 3",
-    },
-    // Add more product details as needed
-  };
-
+    const asin_array = Object.keys(recommendation["res"])
   return (
     <div>
       <h2>Recommended Products</h2>
       <div className="recommended-products-container">
-        {recommendation.map((productId, index) => {
-          const product = productDetails[productId];
+        {
+       asin_array.map((id, index) => {
+        //   const product = recommendation[title];
           return (
-            <div key={index} className="recommended-product">
-              <h3>{recommendation}</h3>
-              <p>Category: {product.category}</p>
+            <div key = {index} className="recommended-product">
+              <h3>{id}</h3>
+              {/* <p>Category: {product.category}</p> */}
             </div>
           );
-        })}
+        })
+        }
       </div>
     </div>
   );
