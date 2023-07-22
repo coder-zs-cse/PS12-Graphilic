67ffacfd689d9c233ec2871e17aa168823a18411
Zubin Shah <wcoderzs@gmail.com>
2023-07-22 05:47:44 +0530

    Heatmap updated

--- Frontend/Employee/heatmap.py
+++ Frontend/Employee/heatmap.py
@@ -8,15 +8,20 @@ reviews_data = pd.read_csv('../../Data/reviews1_data.csv')
 # Assuming the data has the following columns: time, user_id, rating, total_votes, helpfulness_votes
 
 # Data aggregation: group by rating and calculate average helpfulness_votes
-heatmap_data = reviews_data.groupby('month')['rating'].mean().reset_index()
+# filter group Book from reveiws_data
 
-# Reshape the data for heatmap plotting
-heatmap_data = heatmap_data.pivot(index='month', columns='rating', values='rating')
+def getHeatMap(group,month,year):
 
-# Create the heatmap
-plt.figure(figsize=(10, 6))
-sns.heatmap(heatmap_data, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5)
-plt.title('Heatmap of Ratings In Current Trends')
-plt.xlabel('Number of Ratings')
-plt.ylabel('Rating')
-plt.show()
+    heatmap_data = reviews_data[(reviews_data['group'] == group) & (reviews_data['year'] == year)].groupby('month')['rating','helpful'].mean().reset_index()
+    # Reshape the data for heatmap plotting
+    heatmap_data = heatmap_data.pivot(index='month', columns='rating', values='helpful')
+
+    # Create the heatmap
+    plt.figure(figsize=(10, 6))
+    sns.heatmap(heatmap_data, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5)
+    plt.title('Heatmap of Ratings In Current Trends')
+    plt.xlabel('Number of Ratings')
+    plt.ylabel('Rating')
+    plt.show()
+
+getHeatMap('Book',6,2000)
