d2e24e481ae71d9213053046beb9041a117c8015
Zubin Shah <wcoderzs@gmail.com>
2023-07-22 09:31:37 +0530

    Preprocessed Loading options included

--- Frontend/Employee/load_options.py
+++ Frontend/Employee/load_options.py
@@ -1,5 +1,6 @@
 def load_options(df):
     available_days = df['day'].unique()
+    available_months = df['month'].unique()
     available_years = sorted(df['year'].unique().tolist())
     available_cats = df['group'].unique()
-    return available_days,available_years,available_cats
+    return available_days,available_months,available_years,available_cats
